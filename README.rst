74cf6500ef0a65c5247e5923d8cc5422552cd1d8
Christopher Martin <chris.martin@gatech.edu>
2011-06-28 22:28:53 -0400

    Initial code commit: post_long and get_long_range Node targets.
    Also added example usage of these two functions to the readme.

--- README.rst
+++ README.rst
@@ -3,10 +3,38 @@ Neo4j ValueContext Plugin
 
 Numeric index values in Neo4j can be manipulated trivially using ValueContext_,
 but this functionality is not currently exposed to the REST API.
-
 Neo4j ValueContext Plugin extends the `Neo4j REST service`_ to permit indexing of numeric values.
 
-.. _ValueContext: http://api.neo4j.org/current/org/neo4j/index/lucene/ValueContext.html#numeric(java.lang.Number)
+.. _ValueContext: http://api.neo4j.org/current/org/neo4j/index/lucene/ValueContext.html
 
 .. _Neo4j REST service: http://components.neo4j.org/neo4j-server/milestone/rest.html
+
+Example Usage
+-------------
+
+Create a "time" index::
+
+ curl -X DELETE http://localhost:7474/db/data/index/node/time
+ curl -X POST -H Accept:application/json -HContent-Type:application/json -d \
+   '{"name":"time", "config":{"type":"exact","provider":"lucene"}}' \
+   http://localhost:7474/db/data/index/node
+
+Give the reference node a timestamp value of 25::
+
+ curl -H Accept:application/json http://localhost:7474/db/data/ext/Catnap/node/0/post_long \
+   -H "Content-Type: application/json" -d '{"index": "time", "key":"timestamp", "value":25}'
+ curl -H Accept:application/json \
+   http://localhost:7474/db/data/index/node/time/timestamp/25
+
+Query the time index for nodes with timestamps between 1 and 8.
+The result is empty because 25 ∉ [1, 8]::
+
+ curl -H Accept:application/json http://localhost:7474/db/data/ext/Catnap/node/0/get_long_range \
+   -H "Content-Type: application/json" -d '{"index": "time", "key":"timestamp", "min":1, "max":8}'
+
+Query the time index for nodes with timestamps between 8 and 30.
+The result contains the ref node because 25 ∈ [8, 30]::
+
+ curl -H Accept:application/json http://localhost:7474/db/data/ext/Catnap/node/0/get_long_range \
+   -H "Content-Type: application/json" -d '{"index": "time", "key":"timestamp", "min":8, "max":30}'
 
---
+++ src/main/java/ly/scholr/neo4j/valuecontext/ValueContextPlugin.java
@@ -0,0 +1,67 @@
+package ly.scholr.neo4j.valuecontext;
+
+import org.apache.lucene.search.NumericRangeQuery;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.index.lucene.QueryContext;
+import org.neo4j.index.lucene.ValueContext;
+import org.neo4j.server.plugins.*;
+
+import java.util.Arrays;
+
+public class ValueContextPlugin extends ServerPlugin {
+
+  @Name("post_long")
+  @PluginTarget(Node.class)
+  public Iterable<Node> postLong(
+      @Source Node node,
+      @Parameter(name = "index") String indexName,
+      @Parameter(name = "key") String key,
+      @Parameter(name = "value") Long value) {
+
+    GraphDatabaseService graphDb = node.getGraphDatabase();
+    Index<Node> index = graphDb.index().forNodes(indexName);
+
+    Transaction tx = graphDb.beginTx();
+    try {
+      index.add(node, key, new ValueContext(value).indexNumeric());
+      tx.success();
+    } finally {
+      tx.finish();
+    }
+    return Arrays.asList();
+  }
+
+  @Name("get_long_range")
+  @PluginTarget(Node.class)
+  public Iterable<Node> getLongRange(
+      @Source Node node,
+      @Parameter(name = "index") String indexName,
+      @Parameter(name = "key") String key,
+      @Parameter(name = "min") Long minNullable,
+      @Parameter(name = "max") Long maxNullable ) {
+
+    long min = minNullable == null ? Long.MIN_VALUE : minNullable;
+    long max = maxNullable == null ? Long.MAX_VALUE : maxNullable;
+    Sort sort = new Sort(new SortField(key, SortField.LONG, false));
+    Object query = NumericRangeQuery.newLongRange(key, min, max, true, true);
+
+    GraphDatabaseService graphDb = node.getGraphDatabase();
+    Index<Node> index = graphDb.index().forNodes(indexName);
+
+    Iterable<Node> hits;
+    Transaction tx = graphDb.beginTx();
+    try {
+      hits = index.query(key, new QueryContext(query).sort(sort));
+      tx.success();
+    } finally {
+      tx.finish();
+    }
+    return hits;
+  }
+
+}
